e049718e01391563e8853fe68082ac38c3bc650c
Andre Santiago <the.asantiagojr@gmail.com>
2018-06-15 15:19:38 +0000

    automatically backed up by learn

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -15,13 +15,13 @@ def greeting(greeting, name)
 end
 
 def return_a_value
-  puts "Nice"
+  return "Nice"
 end
 
 def last_evaluated_value
-
+  return "expert"
 end
 
-def pizza_party
-
+def pizza_party(topping = "cheese")
+  puts topping
 end
